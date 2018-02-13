8a1c923f4335c23ddf2f985d752830f0b884a88a
Alex <petrewoo@hotmail.com>
2018-02-13 15:49:07 +0800

    T3: Class-based Views.
    step1: rewriting out api using class-based views.

--- snippets/urls.py
+++ snippets/urls.py
@@ -8,8 +8,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    url('^snippets/$', views.snippet_list),
-    url('^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail)
+    url('^snippets/$', views.SnippetList.as_view()),
+    url('^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
