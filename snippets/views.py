8fe92abaf7fb9ec6336a09135b8e93362c1ea135
Alex <petrewoo@hotmail.com>
2018-02-13 16:36:22 +0800

    T3: Class-based Views.
    step3: using generic class-based views.

--- snippets/views.py
+++ snippets/views.py
@@ -4,37 +4,16 @@ from __future__ import unicode_literals
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
-from rest_framework import mixins
 from rest_framework import generics
 
 # Create your views here.
 
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, reqeust, *args, **kwargs):
-        return self.list(reqeust, *args, **kwargs)
 
-    def post(self, reqeust, *args, **kwargs):
-        return self.create(reqeust, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
