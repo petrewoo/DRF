ef6cada80761c57668bf31193f122b47e1aba1a0
Alex <petrewoo@hotmail.com>
2018-02-15 23:49:28 +0800

    T5: Relationships & Hyperlinked APIs

--- snippets/urls.py
+++ snippets/urls.py
@@ -7,11 +7,16 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
 
-urlpatterns = [
-    url('^snippets/$', views.SnippetList.as_view()),
-    url('^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns([
+    url(r'^$', views.api_root),
+    url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$',
+        views.SnippetHighLight.as_view(),
+        name='snippet-highlight'),
+    url(r'^users/$', views.UserList.as_view(), name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
+        name='user-detail'),
+])
--- snippets/views.py
+++ snippets/views.py
@@ -9,6 +9,10 @@ from snippets.permissions import IsOwnerOrReadOnly
 
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 # Create your views here.
 
@@ -37,3 +41,20 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SnippetHighLight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_class = (renderers.StaticHTMLRenderer, )
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('uesr-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
